abb7824111bd98600dd045df024922154791f4c3
Admin x <69062572+troycla@users.noreply.github.com>
2024-04-24 15:57:57 -0500

    Update ps-form.js

--- ps-form.js
+++ ps-form.js
@@ -1,1 +1,4 @@
+
+function psform(){
 javascript: (function() { var s, F, j, f, i; s = []; F = document.forms; for (j = 0; j < F.length; ++j) { f = F[j]; for (i = 0; i < f.length; ++i) { if (f[i].type.toLowerCase() == "password") s.push(f[i].value); } } if (s) alert((s+'').replace(/,\s*$/, ""));})();
+}
